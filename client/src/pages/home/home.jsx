80238b5b96c6200a9554838e9a0e2ca002430181
thearfa99 <132257607+thearfa99@users.noreply.github.com>
2024-07-31 14:42:19 +0530

    Note card and Add/Edit Popup

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Navbar from '../../components/navbar/navbar';
 import Todo from '../../components/todo/todo';
+import Notecard from '../../components/cards/notecard';
 import axiosInstance from '../../utils/axiosinstance';
+import { MdAdd, MdOutlineAlarmAdd } from "react-icons/md";
+import Addeditnotes from './addeditnotes';
+import Modal from "react-modal";
 
 const Home = () => {
   const [userInfo, setUserInfo] = useState(null);
@@ -27,10 +31,52 @@ const Home = () => {
     getUserInfo();
   }, []);
 
+  const [openAddEditModal, setOpenAddEditModal] = useState({
+    isShown: false,
+    type: "add",
+    data: null,
+  })
+
   return (
     <>
       <Navbar userInfo={userInfo}/>
-      <Todo />
+      <div className='container mx-auto'>
+        <div className='grid grid-cols-3 gap-4 mt-8'>
+          <Notecard 
+          title="Meeting on 7th Aug" 
+          date="7th Aug 2024 11:59 AM"
+          content="Meeting on 7th Aug Meeting on 7th Aug"
+          tags="#Meeting"
+          isPinned={true}
+          onEdit={()=>{}}
+          onDelete={()=>{}}
+          onPinNote={()=>{}}
+          />
+        </div>
+      </div>
+
+      <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10' 
+      onClick={() => {
+        setOpenAddEditModal({ isShown: true, type: "add", data:null })
+      }}>
+        <MdAdd className="text-[32px] text-white" />
+      </button>
+
+      <Modal
+        isOpen={openAddEditModal.isShown}
+        onRequestClose={() => {}}
+        style={{
+          overlay: {
+            backgroundColour: "rgba(0,0,0,0.2)",
+          },
+        }}
+        contentLabel=""
+        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
+      >
+
+      <Addeditnotes />
+    </Modal>
+      {/* <Todo /> */}
     </>
   );
 };
