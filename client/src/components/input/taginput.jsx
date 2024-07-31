147d24ff7683f563de912816cc2b77350133fb03
thearfa99 <132257607+thearfa99@users.noreply.github.com>
2024-07-31 16:13:27 +0530

    Update taginput.jsx

--- client/src/components/input/taginput.jsx
+++ client/src/components/input/taginput.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { MdAdd } from 'react-icons/md'
 
 const taginput = () => {
+    const [inputValue, setInputValue] = useState("");
+
+    const handleInputChange =(e) => {
+        setInputValue(e.target.value);
+    };
+
+    const addNewTag = () => {
+        if (inputValue.trim() !=="") {
+            setTags([...tags, inputValue.trim()]);
+            setInputValue("");
+        }
+    }
+
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+            addNewTag();
+        }
+    }
   return (
     <div>
-      TagInput
+        <div className='flex items-center gap-4 mt-3'>
+            <input 
+            type="text" 
+            className='text-sm bg-transparent border px-3 py-2 rounded outline-none' 
+            placeholder='Add Tags'
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+            />
+
+            <button className='w-8 h-8 flex items-center justify-center rounded border border-blue-700 hover:bg-blue-700'>
+                <MdAdd className='text-2xl text-blue-700 hover:text-white'/>
+            </button>
+        </div>
     </div>
   )
 }
