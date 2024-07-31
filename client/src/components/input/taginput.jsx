3ebae707d160043304320d826606c39f9ec42169
thearfa99 <132257607+thearfa99@users.noreply.github.com>
2024-07-31 17:24:43 +0530

    Tags input UI

--- client/src/components/input/taginput.jsx
+++ client/src/components/input/taginput.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
-import { MdAdd } from 'react-icons/md'
+import { MdAdd, MdClose } from 'react-icons/md'
 
-const taginput = () => {
+const taginput = ({ tags, setTags }) => {
     const [inputValue, setInputValue] = useState("");
 
-    const handleInputChange =(e) => {
+    const handleInputChange = (e) => {
         setInputValue(e.target.value);
     };
 
     const addNewTag = () => {
-        if (inputValue.trim() !=="") {
+        if (inputValue.trim() !== "") {
             setTags([...tags, inputValue.trim()]);
             setInputValue("");
         }
@@ -20,8 +20,30 @@ const taginput = () => {
             addNewTag();
         }
     }
+
+    const handleRemoveTag = (tagToRemove) => {
+        setTags(tags.filter((tag) => tag !== tagToRemove))
+    }
+
   return (
     <div>
+        {tags?.length > 0 && (
+            <div className='flex items-center gap-2 flex-wrap mt-2'>
+                {tags.map((tag, index) => (
+                    <span key={index} className=''>
+                        # {tag}
+                        <button 
+                            onClick={() => {
+                                handleRemoveTag(tag);
+                            }}
+                        >
+                            <MdClose />
+                        </button>
+                    </span>
+                ))}
+            </div>
+        )}
+
         <div className='flex items-center gap-4 mt-3'>
             <input 
             type="text" 
@@ -31,7 +53,10 @@ const taginput = () => {
             onKeyDown={handleKeyDown}
             />
 
-            <button className='w-8 h-8 flex items-center justify-center rounded border border-blue-700 hover:bg-blue-700'>
+            <button className='w-8 h-8 flex items-center justify-center rounded border border-blue-700 hover:bg-blue-700'
+                onClick={()=>{
+                    addNewTag();
+                }}>
                 <MdAdd className='text-2xl text-blue-700 hover:text-white'/>
             </button>
         </div>
--- client/src/pages/home/addeditnotes.jsx
+++ client/src/pages/home/addeditnotes.jsx
@@ -1,31 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Taginput from '../../components/input/taginput'
 
 const addeditnotes = () => {
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [tags, setTags] = useState([]);
+
   return (
     <div>
         <div className='flex flex-col gap-2'>
             <label className='input-label'>TITLE</label>
             <input
-            type="text"
-            className='text-2xl text-slate-950 outline-none'
-            placeholder='Add a Title'
+                type="text"
+                className='text-2xl text-slate-950 outline-none'
+                placeholder='Add a Title'
+                value={title}
+                onChange={({ target }) => setTitle(target.value)}
             />
         </div>
 
         <div className='flex flex-col gap-2 mt-4'>
             <label className='input-label'>CONTENT</label>
             <textarea
-            type="text"
-            className='text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded'
-            placeholder='Content'
-            rows={10}
+                type="text"
+                className='text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded'
+                placeholder='Content'
+                rows={10}
+                value={content}
+                onChange={({ target }) => setContent(target.value)}
             />
         </div>
 
         <div className='mt-3'>
             <label className='input-label'>TAGS</label>
-            <Taginput />
+            <Taginput tags={tags} setTags={setTags} />
         </div>
 
         <button className='btn-primary font-medium mt-5 p-3' onClick={() => {}}>
