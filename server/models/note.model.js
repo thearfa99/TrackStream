47269cc9ac6bf962103662c043769e741c3a479e
thearfa99 <132257607+thearfa99@users.noreply.github.com>
2024-08-05 14:02:03 +0530

    Update note.model.js

--- server/models/note.model.js
+++ server/models/note.model.js
@@ -1,17 +1,19 @@
 const mongoose = require("mongoose");
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
 
 const Schema = mongoose.Schema;
 
 const noteSchema = new Schema({
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    tags: { type: [String], default: [] },
-    isPinned: { type: Boolean, required: false },
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    createdTime: { type: Date, default: Date.now },
+    title: { type: String, required: true }, // Title of Task
+    content: { type: String, required: true }, // Description
+    tags: { type: [String], default: [] }, // Tags for easy search
+    isPinned: { type: Boolean, required: false }, // Enables pinning
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // User creating the task
+    createdTime: { type: Date, default: Date.now }, 
     completedTime: { type: Date },
-    status: { type: String, required: false },
+    status: { type: String, required: false }, // To-do, In progress, Complete
+    priority: { type: String, required: false }, // Deafault, Low Priority, High Priority
+    assignedUsers: { type: String, required: false }, // Assigned to Users or general task
 });
 
 module.exports = mongoose.model("Note", noteSchema);
