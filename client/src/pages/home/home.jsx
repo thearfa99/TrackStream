309fe3eb57e72951465db0bf2387d3ea2fbc7314
thearfa99 <132257607+thearfa99@users.noreply.github.com>
2024-09-06 12:57:03 +0530

    toast L

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -169,7 +169,7 @@ const Home = () => {
         handleClearSearch={handleClearSearch}
       />
 
-<div className='container mx-auto mt-8'>
+      <div className='container mx-auto mt-8'>
         <div className='flex justify-between items-center mb-8'>
           <h1 className='text-3xl font-semibold'>Project Dashboard</h1>
           <button 
